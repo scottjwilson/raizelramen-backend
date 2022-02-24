d3162600662e94d317d5f2ac1478b39461393b40
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-02-24 06:47:04 -0800

    ok

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
     provider: "nodemailer",
     providerOptions: {
       host: env("SMTP_HOST", "smtp.gmail.com"),
-      port: env("SMTP_PORT", 587),
+      port: env("SMTP_PORT", 465),
+
       auth: {
         user: env("SMTP_USERNAME"),
         pass: env("SMTP_PASSWORD"),
